e6c38f3fe414e32930034f85c50d19d6a63ae933
Nitesh Shah <Fineoutput2021>
2022-07-06 13:44:21 +0530

    banner image error fix

--- application/controllers/f9admin/Banner_image.php
+++ application/controllers/f9admin/Banner_image.php
@@ -235,7 +235,7 @@
                        if (!file_exists($image_upload_folder)) {
                            mkdir($image_upload_folder, DIR_WRITE_MODE, true);
                        }
-                       $new_file_name="banner_image".date("Ymdhms");
+                       $new_file_name="banner_image2".date("Ymdhms");
                        $this->upload_config = array(
                              'upload_path'   => $image_upload_folder,
                              'file_name' => $new_file_name,
